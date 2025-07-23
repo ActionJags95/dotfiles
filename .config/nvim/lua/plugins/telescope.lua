fd0a080b4370265b3fa897cc48cb9533459a11ea
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-24 03:03:03 +0530

    Added telescope-ui-select plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,42 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            -- Configure mappings
+          },
+        }
+      })
+    end
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
 
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local builtin = require("telescope.builtin")
-
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          -- Configure mappings
-        },
-
+            }
+          }
+        }
       }
-    })
-  end
+
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
