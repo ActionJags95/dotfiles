b9e7965a8df8840fa8b8a613ffefcfb89f3b1b54
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 23:05:55 +0530

    Added indentation keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -36,3 +36,7 @@ vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+
+-- Indentation
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", "<", ">gv", opts)
