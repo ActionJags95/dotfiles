51d0edd47fdbb7bb5b052f4195ca91bad7a7386b
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 20:18:14 +0530

    Some more options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -6,4 +6,9 @@ opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.number = true
 opt.relativenumber = true
-
+opt.clipboard = "unnamedplus"
+opt.linebreak = true
+opt.wrap = false
+opt.autoindent = true
+opt.ignorecase = true
+opt.smartcase = true
