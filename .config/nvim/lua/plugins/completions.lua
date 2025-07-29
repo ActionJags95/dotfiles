fd33caef4fed589fe143ea7e99404ce25871207f
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 23:06:17 +0530

    Rounded border and custom keymap to match VS Code

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,35 +1,59 @@
 return {
   {
-    'saghen/blink.cmp',
+    "saghen/blink.cmp",
     dependencies = {
-      'rafamadriz/friendly-snippets',
+      "rafamadriz/friendly-snippets",
     },
-    version = '1.*',
+    version = "1.*",
 
     --@module 'blink.cmp'
     --@type blink.cmp.config
 
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "none",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+      },
 
       appearance = {
-        nerd_font_variant = 'normal'
+        nerd_font_variant = "normal",
       },
 
       cmdline = {
-        keymap = { preset = "inherit" },
+        keymap = {
+          preset = "none",
+        },
         completion = {
-          menu = { auto_show = true},
+          menu = { auto_show = true },
           ghost_text = { enabled = true },
         },
       },
 
       completion = {
-        keyword = { range = 'full' },
+        keyword = { range = "full" },
 
-        documentation = { auto_show = true, auto_show_delay_ms = 0 },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+          {
+            border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+          },
+        },
 
-        ghost_text = { enabled = true },
+        ghost_text = { enabled = false },
 
         -- Disable auto brackets
         accept = { auto_brackets = { enabled = false } },
@@ -38,25 +62,26 @@ return {
           -- Don't automatically show the completion menu
           auto_show = true,
 
+          -- Rounded borders
+          border = "rounded",
+
           -- nvim-cmp style completion menu
           draw = {
             columns = {
               { "label", "label_description", gap = 1 },
-              {"kind_icon", "kind" }
+              { "kind_icon", "kind" },
             },
           },
         },
-
-
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
 
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
 
-    opts_extended = { "sources.default" }
+    opts_extended = { "sources.default" },
   },
 }
