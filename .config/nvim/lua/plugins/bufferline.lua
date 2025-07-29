1017fd6995bd1fcec2acd7a10da14e41ce7d27f7
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 20:20:12 +0530

    Removed keyamaps and revamped to VSCode like UI

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,15 +1,27 @@
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
 
-  config = function()
-    local bufferline = require("bufferline")
-    bufferline.setup({
-      options = {
-        mode = "buffers",
-        style_preset = bufferline.style_preset.default,
-        separator_style = "slant",
-      }
-    })
-  end
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				style_preset = bufferline.style_preset.default,
+				separator_style = "slant",
+
+				-- Offset to have VS Code like UI
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "FileExplorer",
+						highlight = "Directory",
+						text_align = "left",
+						seperator = true,
+						separator_style = "slant",
+					},
+				},
+			},
+		})
+	end,
 }
