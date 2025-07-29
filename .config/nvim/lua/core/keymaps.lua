8a9d98019ad1704b7bf34c4c1e8712c130974c54
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 20:17:59 +0530

    Some more keymaps

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,4 +1,38 @@
-local opts = { noremap = true, silent = true }
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Disable spacebar key's default behaviour in Normal and Visual mode
+vim.keymap.set({ "n", "v" }, "<Sapce>", "<Nop>", { silent = true })
+
+-- For concisness
+local opts = { noremap = true, silent = true }
+
+--save file
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+
+--quit file
+vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
+
+-- resize with arrows
+vim.keymap.set("n", "<Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<Right>", ":resize +2<CR>", opts)
+
+-- Buffers
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", opts)
+vim.keymap.set("n", "<leader>bp", ":bprev<CR>", opts)
+vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", opts)
+vim.keymap.set("n", "<leader>bc", "<cmd> enew <CR>", opts)
+
+-- Window management
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts)
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts)
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts)
+vim.keymap.set("n", "<leader>xs", ":close<CR>", opts)
+
+-- Navigate between splits
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
