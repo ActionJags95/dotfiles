5726c0f41aaa9e3a41f372acdb1d1e6f535c8cdd
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 20:23:30 +0530

    Integrated with blink.cmp and auto tools installation

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,51 +1,84 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "clangd", "cssls", "cssmodules_ls", "dockerls", "pyright", "tailwindcss", "yamlls", "taplo", "html" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      'saghen/blink.cmp',
-    },
-    opts = {
-      servers = {
-        lua_ls = {}
-      }
-    },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
-      -- Setting the LSP
-      lspconfig.lua_ls.setup { capabilities = capabilities }
-      lspconfig.ts_ls.setup { capabilities = capabilities }
-      lspconfig.clangd.setup { capabilities = capabilities }
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+			"WhoIsSethDaniel/mason-tool-installer",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"clangd",
+					"cssls",
+					"cssmodules_ls",
+					"dockerls",
+					"pyright",
+					"tailwindcss",
+					"yamlls",
+					"taplo",
+					"html",
+					"emmet_ls",
+				},
+				automatic_installation = true,
+			})
 
-      -- Inline code warnings
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+					"isort",
+					"pylint",
+					{ "eslint_d", version = "13.1.2" },
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+		},
+		opts = {
+			servers = {
+				lua_ls = {},
+			},
+		},
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			local lspconfig = require("lspconfig")
+			-- Setting the LSP
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.emmet_ls.setup({ capabilities = capabilities })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			-- Disabline inline diagnostics
+			vim.diagnostic.config({
+				virtual_text = true,
+				update_in_insert = true,
+				underline = true,
+				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
