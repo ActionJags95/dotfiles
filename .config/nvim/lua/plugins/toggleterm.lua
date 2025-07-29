648ed0dfb0282bd3f17f222222a116e9ba2caa06
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-29 20:29:42 +0530

    Added kuymaps

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,7 +1,9 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {
-
-  },
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		vim.keymap.set("n", "<leader>tth", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true }),
+		vim.keymap.set("n", "<leader>ttv", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true }),
+		vim.keymap.set("n", "<leader>ttf", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true }),
+	},
 }
