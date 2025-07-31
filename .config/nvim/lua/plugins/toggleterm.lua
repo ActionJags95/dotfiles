096fc7e99aa71dcac87c178f239111ea9fa120b8
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-31 07:37:16 +0530

    Simpler keymaps

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,9 +1,24 @@
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	opts = {
-		vim.keymap.set("n", "<leader>tth", ":ToggleTerm direction=horizontal<CR>", { noremap = true, silent = true }),
-		vim.keymap.set("n", "<leader>ttv", ":ToggleTerm direction=vertical<CR>", { noremap = true, silent = true }),
-		vim.keymap.set("n", "<leader>ttf", ":ToggleTerm direction=float<CR>", { noremap = true, silent = true }),
-	},
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    vim.keymap.set(
+      "n",
+      "<leader>th",
+      ":ToggleTerm direction=horizontal<CR>",
+      { noremap = true, silent = true, desc = "Toggle horizontal terminal" }
+    ),
+    vim.keymap.set(
+      "n",
+      "<leader>tv",
+      ":ToggleTerm direction=vertical<CR>",
+      { noremap = true, silent = true, desc = "Toggle vertical terminal" }
+    ),
+    vim.keymap.set(
+      "n",
+      "<leader>tf",
+      ":ToggleTerm direction=float<CR>",
+      { noremap = true, silent = true, desc = "Toggle float terminal" }
+    ),
+  },
 }
