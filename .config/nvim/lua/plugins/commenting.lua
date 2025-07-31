2a7b6b7a1f0fb57a11985ac1150c076c61e47f05
ActionJags95 <118545266+ActionJags95@users.noreply.github.com>
2025-07-31 07:31:50 +0530

    VS Code style keymaps

--- .config/nvim/lua/plugins/commenting.lua
+++ .config/nvim/lua/plugins/commenting.lua
@@ -7,28 +7,31 @@ return {
     },
 
     config = function()
-      require("ts_context_commentstring").setup {
+      require("ts_context_commentstring").setup({
         enable_autocmd = false,
-      }
+      })
 
-      require("mini.comment").setup {
+      require("mini.comment").setup({
         -- tsx, jsx, html, svelte comment support
         options = {
           custom_commentstring = function()
-            return require("ts_context_commentstring.internal").calculate_commentstring({ key = 
-              'commentstring'})
-              or vim.bo.commentstring
-            end,
+            return require("ts_context_commentstring.internal").calculate_commentstring({
+              key = "commentstring",
+            }) or vim.bo.commentstring
+          end,
         },
-      }
-    end
+
+        mappings = {
+          comment_line = "<C-_>",
+          comment_visual = "<C-_>",
+        },
+      })
+    end,
   },
 
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-
-    },
+    opts = {},
   },
 }
